e8e1ddb404f74e07aeac812ee5825de607f6b405
Neutrino <dipaoloclaudiax@gmail.com>
2024-06-28 13:18:50 +0200

    page structure

--- source/2 database.sql
+++ source/2 database.sql
@@ -5,20 +5,6 @@ create table iscrizione (
     DataDiNascita date not null,
     telefono bigint not null,
     classe varchar(40) not null,
-    messaggio varchar(600),
     primary key (email)
 );
 
-create table help (
-    email varchar(40) not null, 
-    problema varchar(600) not null
-
-);
-
-create table collabora (
-    cognome varchar(40),
-    nome varchar(40),
-    email varchar(40) not null,
-    descrizione varchar(40) not null
-
-)
