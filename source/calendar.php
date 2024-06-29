9f5e94359f5a5c98648e291dd685373c93bd488e
ValerioBaldi <valerio.baldi11@gmail.com>
2024-06-30 01:03:27 +0200

    Minor update, just implemented a simple calendar

---
+++ source/calendar.php
@@ -0,0 +1,146 @@
+<?php
+$host = "localhost";
+$dbname = "postgres";
+$user = "postgres";
+$password = "postgres";
+
+$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
+
+// Query per recuperare i dati
+$queryHeadaches = "SELECT rid AS rid, starting_time AS start, ending_time AS end FROM headache_report";
+$queryDigital = "SELECT rid AS rid, starting_time AS start, ending_time AS end FROM digital_report";
+$querySleeping = "SELECT rid AS rid, starting_time AS start, ending_time AS end FROM sleeping_report";
+
+$resultHeadaches = pg_query($conn, $queryHeadaches);
+$resultDigital = pg_query($conn, $queryDigital);
+$resultSleeping = pg_query($conn, $querySleeping);
+
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+pg_close($conn);
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario 2020-2025</title>
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.css' rel='stylesheet' />
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.js'></script>
+    <link rel="stylesheet" href="1 bootstrap/css/bootstrap.css" />
+    <script src="1 bootstrap/js/bootstrap.bundle.js"></script>
+    <link rel="icon" href="2 images/senzabordo.png">
+    <link rel = "stylesheet" href = "style.css" />
+    <link rel = "stylesheet" href = "digital.css" />
+</head>
+<body>
+
+    <!-- navbar in alto -->
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="coffee.html">Coffee recordings</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="digital.html">Digital Devices Recordings</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="calendar.php">Personal Report Calendar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="settings.html">Settings</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+    
+        </nav>
+
+
+    <br>
+    <br>
+        <!--titoletto-->
+    
+    <strong><h1 class="fw-bolder text-center" style="color:rgb(80, 197, 255)">Digital Devices Recording</h1></strong>
+    
+    
+    <img src="2 images/logo.png" class="top-left-logo">
+
+
+    <!-- language selection -->
+        <div class="language">
+        <br><br><br><br>
+        <a class="nav-link dropdown-toggle" href="#" role="button"
+        data-bs-toggle="dropdown" aria-expanded="false">
+        Language
+        </a>
+        <ul class="dropdown-menu text-center">
+        <li><a class="dropdown-item" href="chisiamo.html">English</a></li>
+        <li><a class="dropdown-item" href="dovesiamo.html">Italiano</a></li>
+        </ul>
+        </div>
+        <br><br>
+
+        <!-- info point -->
+        <img src="2 images/question.png" alt="description" class="bottom-right-image">
+
+    <!-- logged in as -->
+    <h6 class="bottom-left-logged">Logged in as:</h6>
+
+    <div id='calendar'></div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+
+            /*var headacheReports= [
+                <?php 
+                foreach($headache_report as $report){
+                    echo "{start:  '" . $report['start'] . "', end: '" . $report['end'] . "', id: '" . $report['rid'] ."'},";
+                }
+                ?>
+            ];*/
+            
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                initialDate: '2020-01-01',
+                headerToolbar: {
+                    left: 'prevYear,prev,next,nextYear today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: true,
+                navLinks: true, // can click day/week names to navigate views
+                dateClick: function(info) {
+                    alert('Clicked on: ' + info.dateStr);
+                }
+                /*dayCellDidMount: function(info) {
+                    for (var i = 0; i < headacheReports.length; i++) {
+                        var report = new Date(datesToHighlight[i]);
+                        if (info.date.getTime() === dateToHighlight.getTime()) {
+                            info.el.style.backgroundColor = 'lightblue';
+                        }
+                    }
+                },*/
+            });
+            calendar.render();
+        });
+    </script>
+</body>
+</html>
