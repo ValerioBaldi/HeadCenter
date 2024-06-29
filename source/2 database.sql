2194bd222cd76e1084eae0c79c83f6b5e9b49d28
Neutrino <dipaoloclaudiax@gmail.com>
2024-06-29 17:26:07 +0200

    database

--- source/2 database.sql
+++ source/2 database.sql
@@ -1,10 +1,38 @@
-create table iscrizione (
-    email varchar(40) not null, 
-    nome varchar(40) not null,
-    cognome varchar(40) not null,
-    DataDiNascita date not null,
-    telefono bigint not null,
-    classe varchar(40) not null,
-    primary key (email)
+create table digital (
+    report_id int NOT NULL,
+    digital_usage_from timestamp NOT NULL,
+    digital_usage_to timestamp NOT NULL,
+    symptoms char(100),
+    devices_type char(40) NOT NULL,
+    usage_for char(30) NOT NULL
 );
+
+create table sleep (
+    report_id int NOT NULL,
+    sleeping_time_from timestamp NOT NULL,
+    sleeping_time_to timestamp NOT NULL,
+    coffee_cups int NOT NULL,
+    sleeping_rate int NOT NULL,
+    awaken_during_sleep boolean NOT NULL
+)
+
+create table headache (
+    report_id int NOT NULL,
+    starting_time timestamp NOT NULL,
+    ending_time timestamp,
+    ache_position char(20),
+    ache_intensity char(10),
+    ache_type char(20),
+    painkillers char(100),
+    repercussions char(100),
+    symptoms char(100),
+    notes char(200)
+)
+
+create table user (
+    id int NOT NULL,
+    first_name char(50) NOT NULL,
+    surname char(50) NOT NULL,
+    age int NOT NULL
+)
 
