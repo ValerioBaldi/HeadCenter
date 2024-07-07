a20196fb14f8a42338e700800695045af18e5627
unknown <dipaoloclaudiax@gmail.com>
2024-07-07 13:39:59 +0200

    coffee html and php

--- source/coffee.php
+++ source/coffee.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] !="POST") { /* verifica richiesta post */
 else {
    /*  connessione */
 $dbconn = pg_connect("host=localhost port=5432 dbname=postgres
-        user=postgres password=postgres")
+        user=postgres password=123456")
         or die('Could not connect: ' . pg_last_error());
 }
 
@@ -34,9 +34,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $sleeping_time_from = $sleeping_date . ' ' . $from;
                 $sleeping_time_to = $sleeping_date . ' ' . $to;
 
-                $q2 = "insert into sleep values ($1, $2, $3, $4, $5, $6, $7)";
+                $q2 = "insert into sleep values ($1, $2, $3, $4, $5, $6, $7, $8)";
                 $data=pg_query_params($dbconn, $q2,
-                        array($report_id, $sleeping_date, $sleeping_time_from, $sleeping_time_to,
+                        array($report_id, $report_by, $sleeping_date, $sleeping_time_from, $sleeping_time_to,
                         $coffee_cups, $sleeping_rate, $awaken_during_sleep));
                 if ($data) {
                     header("Location: okpag.html") ;
